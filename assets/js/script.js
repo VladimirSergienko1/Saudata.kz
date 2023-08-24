8dd07c19bab6dbb47f4f6327dc8830ef234779db
vladimir <vladimir.945g@gmail.com>
2023-08-24 12:30:43 +0600

    blocks improvements + carousel added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-
 console.log('Hello')
 
-const defaultSelect = () =>{
-    const element = document.querySelector('.default')
-    const choices = new Choices(element,{
-        searchEnabled: false,
-        placeholderValue: null,
-        searchPlaceholderValue: null,
-    })
-};
+$(document).ready(function() {
 
-defaultSelect()
+
+
+    $('.js-example-basic-single').select2({minimumResultsForSearch: Infinity});
+
+    $('.js-example-basic-single').on('select2:open', function(e) {
+        $(this).parent().find('.select2-selection__arrow::before').css('transform', 'translateY(-50%) rotate(180deg)');
+    });
+
+    $('.js-example-basic-single').on('select2:close', function(e) {
+        $(this).parent().find('.select2-selection__arrow::before').css('transform', 'translateY(-50%)');
+    });
+
+    $('#slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // arrows: true,
+    });
+
+
+
+
+
+});
