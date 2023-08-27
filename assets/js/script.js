2785281e6f2abcca4d622eb38d9bbe538ec5bf8e
vladimir <vladimir.945g@gmail.com>
2023-08-27 21:23:44 +0600

    slider Done, banner Done, detilize section done

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,31 @@ $(document).ready(function() {
         $(this).parent().find('.select2-selection__arrow::before').css('transform', 'translateY(-50%)');
     });
 
-    $('#slider').slick({
+    $('#slider').on('init', function(event, slick){
+        if (slick.currentSlide === 0) {
+            $('.prev-arrow').css('stroke', '#989898');
+            $('.slick-prev').css('pointer-events', 'none')
+
+        }
+    }).slick({
         slidesToShow: 2,
         slidesToScroll: 2,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
-        // arrows: true,
+        arrows: true,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+    }).on('afterChange', function(event, slick, currentSlide){
+        console.log('BLA',currentSlide)
+        if (currentSlide === 0) {
+            $('.prev-arrow').css('stroke', '#989898');
+            $('.slick-prev').css('pointer-events', 'none')
+
+        } else {
+            $('.prev-arrow').css('stroke', '#333333');
+            $('.slick-prev').css('pointer-events', 'all')
+        }
     });
-
-
 
 
 
